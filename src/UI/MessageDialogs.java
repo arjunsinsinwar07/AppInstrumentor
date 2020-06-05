ba99e9d6405c3ec2edd4bd69843ed67f083705ad
arjunsinsinwar07 <51737223+arjunsinsinwar07@users.noreply.github.com>
2020-06-06 00:15:26 +0530

    Initial commit

---
+++ src/UI/MessageDialogs.java
@@ -0,0 +1,114 @@
+package UI;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class MessageDialogs {
+	private ProgressMonitorDialog progressDialog;
+
+	public String openInputDialogAandGetValue(String dialogTitle, String dialogContent, String defaultValue) {
+		InputDialog input = new InputDialog(Display.getCurrent().getActiveShell(), dialogTitle, dialogContent,
+				defaultValue, null);
+		if (input.open() != InputDialog.OK) {
+			return null;
+		}
+		return input.getValue();
+	}
+
+	public boolean openConfirmDialog(String title, String message) {
+		boolean result = MessageDialog.openConfirm(Display.getCurrent().getActiveShell(), title, message);
+		return result;
+	}
+
+	public void openInformationDialog(String title, String message) {
+		MessageDialog.openInformation(Display.getCurrent().getActiveShell(), title, message);
+	}
+
+	public void openErrorDialog(String title, String message) {
+		MessageDialog.openError(Display.getCurrent().getActiveShell(), title, message);
+	}
+
+	public void openErrorDialog(Shell shell, String title, String message) {
+		if (shell.isDisposed()) {
+			return;
+		}
+		MessageDialog.openError(shell, title, message);
+	}
+
+	public void openProgressDialog(Shell shell, String message) {
+		if (shell == null) {
+			shell = Display.getDefault().getActiveShell();
+		}
+		progressDialog = new ProgressMonitorDialog(shell);
+		try {
+			progressDialog.run(false, false, new IRunnableWithProgress() {
+
+				@Override
+				public void run(IProgressMonitor monitor) {
+					monitor.setTaskName(message);
+
+				}
+			});
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		progressDialog.open();
+	}
+
+	public void openProgressDialog(Shell shell, String message, boolean cancelabel, IRunnableWithProgress runnable) {
+		if (shell == null) {
+			shell = Display.getDefault().getActiveShell();
+		}
+		progressDialog = new ProgressMonitorDialog(shell);
+		try {
+			progressDialog.run(true, cancelabel, new IRunnableWithProgress() {
+
+				@Override
+				public void run(IProgressMonitor monitor) {
+					monitor.setTaskName(message);
+					try {
+						runnable.run(monitor);
+					} catch (InvocationTargetException | InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		progressDialog.open();
+	}
+
+	public void openProgressDialogOnBackgroundThread(Shell shell, String message, boolean cancelabel,
+			IRunnableWithProgress runnable) {
+		if (shell == null) {
+			shell = Display.getDefault().getActiveShell();
+		}
+		progressDialog = new ProgressMonitorDialog(shell);
+		try {
+			progressDialog.run(true, cancelabel, runnable);
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		progressDialog.open();
+	}
+
+	public void closeProgressDialog() {
+		this.progressDialog.close();
+	}
+
+	public void executeDisplayAsync(Runnable runnable) {
+		Display.getDefault().asyncExec(runnable);
+	}
+}
---
+++ src/UI/Part.java
@@ -0,0 +1,369 @@
+
+package UI;
+
+import javax.inject.Inject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class Part {
+	private Text textInstrumentorDir;
+	private Text ApkName;
+	private Text InstrumentApkDir;
+	private Text InstrumentedApkName;
+
+	@Inject
+	public Part() {
+
+	}
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setBounds(0, 0, 700, 298);
+
+		Composite composite_1 = new Composite(composite, SWT.NONE);
+		composite_1.setBounds(10, 10, 700, 278);
+
+		Label IntrumentorJarDir = new Label(composite_1, SWT.NONE);
+		IntrumentorJarDir.setBounds(10, 30, 131, 20);
+		IntrumentorJarDir.setText("InstrumentorJarDir");
+
+		textInstrumentorDir = new Text(composite_1, SWT.BORDER);
+		textInstrumentorDir.setBounds(182, 27, 339, 26);
+		textInstrumentorDir.setText("C:\\Users\\Arjun\\Desktop\\NeonifyResources");
+
+		Button btnBrowseins = new Button(composite_1, SWT.NONE);
+		btnBrowseins.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dirDialog = new DirectoryDialog(Display.getDefault().getActiveShell());
+				dirDialog.setFilterPath(textInstrumentorDir.getText());
+				String dir = dirDialog.open();
+
+				if (dir != null)
+					textInstrumentorDir.setText(dir);
+			}
+		});
+		btnBrowseins.setBounds(541, 23, 90, 30);
+		btnBrowseins.setText("Browse");
+
+		Label lblApkname = new Label(composite_1, SWT.NONE);
+		lblApkname.setBounds(10, 73, 70, 20);
+		lblApkname.setText("ApkName");
+
+		ApkName = new Text(composite_1, SWT.BORDER);
+		ApkName.setBounds(181, 70, 340, 26);
+		// ApkName.setText("C:\\Users\\Arjun\\Desktop\\NeonifyResources\\OriginalApks\\AlaskaAirlines.apk");
+
+		Button btnBrowseApk = new Button(composite_1, SWT.NONE);
+		btnBrowseApk.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				FileDialog dirDialog = new FileDialog(Display.getDefault().getActiveShell());
+				dirDialog.setFilterPath(ApkName.getText());
+				String dir = dirDialog.open();
+
+				if (dir != null)
+					ApkName.setText(dir);
+			}
+		});
+		btnBrowseApk.setBounds(541, 66, 90, 30);
+		btnBrowseApk.setText("Browse");
+
+		Label lblInstrumentapkdir = new Label(composite_1, SWT.NONE);
+		lblInstrumentapkdir.setBounds(10, 128, 141, 20);
+		lblInstrumentapkdir.setText("InstrumentedApkDir");
+
+		InstrumentApkDir = new Text(composite_1, SWT.BORDER);
+		InstrumentApkDir.setBounds(182, 125, 339, 26);
+		InstrumentApkDir.setText("C:\\Users\\Arjun\\Desktop\\NeonifyResources\\InstrumentedApks");
+
+		Button btnBrowseInstrumentApkDir = new Button(composite_1, SWT.NONE);
+		btnBrowseInstrumentApkDir.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dirDialog = new DirectoryDialog(Display.getDefault().getActiveShell());
+				dirDialog.setFilterPath(InstrumentApkDir.getText());
+				String dir = dirDialog.open();
+
+				if (dir != null)
+					InstrumentApkDir.setText(dir);
+			}
+		});
+		btnBrowseInstrumentApkDir.setBounds(541, 125, 90, 30);
+		btnBrowseInstrumentApkDir.setText("Browse");
+
+		Label lblNewLabel = new Label(composite_1, SWT.NONE);
+		lblNewLabel.setBounds(10, 176, 153, 20);
+		lblNewLabel.setText("InstrumentedApkName");
+
+		InstrumentedApkName = new Text(composite_1, SWT.BORDER);
+		InstrumentedApkName.setBounds(182, 173, 339, 26);
+		InstrumentedApkName.setText("Test.apk");
+
+		Button btnInstrumentapk = new Button(composite_1, SWT.NONE);
+		btnInstrumentapk.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (validate(textInstrumentorDir.getText(), ApkName.getText(), InstrumentApkDir.getText(),
+						InstrumentedApkName.getText())) {
+					showProgressDialog(textInstrumentorDir.getText(), ApkName.getText(), InstrumentApkDir.getText(),
+							InstrumentedApkName.getText());
+				}
+			}
+		});
+		btnInstrumentapk.setBounds(69, 227, 119, 30);
+		btnInstrumentapk.setText("InstrumentApk");
+
+		Button InstallApp = new Button(composite_1, SWT.NONE);
+		InstallApp.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (validateInstallation(InstrumentApkDir.getText(), InstrumentedApkName.getText()))
+					showProgressDialogForInstall(InstrumentApkDir.getText(), InstrumentedApkName.getText());
+			}
+		});
+		InstallApp.setBounds(217, 227, 90, 30);
+		InstallApp.setText("InstallApk");
+
+		Button browseInstrumentedApk = new Button(composite_1, SWT.NONE);
+		browseInstrumentedApk.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog dirDialog = new FileDialog(Display.getDefault().getActiveShell());
+				dirDialog.setFilterPath(InstrumentedApkName.getText());
+				String dir = dirDialog.open();
+
+				if (dir != null)
+					InstrumentedApkName.setText(dir);
+			}
+
+		});
+		browseInstrumentedApk.setBounds(541, 166, 90, 30);
+		browseInstrumentedApk.setText("Browse");
+
+		Button btnPortfrorwardto = new Button(composite_1, SWT.NONE);
+		btnPortfrorwardto.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					PortForward9899("tcp:9899", "tcp:989");
+				} catch (IOException | InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnPortfrorwardto.setBounds(335, 227, 153, 30);
+		btnPortfrorwardto.setText("PortFrorwardTo9899");
+
+		Button PortForward2 = new Button(composite_1, SWT.NONE);
+		PortForward2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					PortForward9898("tcp:9898", "9899");
+				} catch (IOException | InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		PortForward2.setBounds(517, 227, 153, 30);
+		PortForward2.setText("PortForwardTo9898");
+
+	}
+
+	private void startProcess(String instrumentorDir, String ApkName, String instrumentedApkDir,
+			String instrumentedApkName) throws IOException, InterruptedException {
+		String adbPath = System.getenv("ANDROID_HOME");
+		adbPath = String.valueOf(adbPath) + File.separator + "platform-tools" + File.separator + "adb.exe";
+
+		String instrumentorJarPath = instrumentorDir + File.separator + "Instrumentor.jar";
+		String InstrumentedApkNamePath = instrumentedApkDir + File.separator + instrumentedApkName;
+		String commands[] = { "java", "-jar", instrumentorJarPath, ApkName, InstrumentedApkNamePath };
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		pb.directory(new File(instrumentorDir));
+		File log = new File("log");
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(Redirect.appendTo(log));
+		Process process = pb.start();
+		process.waitFor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
+		String line;
+		while ((line = br.readLine()) != null) {
+
+			System.out.println(line);
+		}
+
+	}
+
+	private void showProgressDialog(final String instrumentorDir, final String apkName, final String instrumentedApkDir,
+			final String instrumentrdApkName) {
+		MessageDialogs msd = new MessageDialogs();
+		msd.openProgressDialog(Display.getDefault().getActiveShell(), "Instrumenting Application! - Please Wait ...",
+				true, new IRunnableWithProgress() {
+			@Override
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				monitor.beginTask("Instrumenting Application! - Please Wait ...", -1);
+
+				try {
+
+					startProcess(instrumentorDir, apkName, instrumentedApkDir, instrumentrdApkName);
+				} catch (IOException | InterruptedException e) {
+
+					e.printStackTrace();
+				}
+
+				monitor.done();
+
+			}
+		});
+		msd.closeProgressDialog();
+	}
+
+	private void showProgressDialogForInstall(final String instrumentedApkDir, final String instrumentrdApkName) {
+		MessageDialogs msd = new MessageDialogs();
+		msd.openProgressDialog(Display.getDefault().getActiveShell(), "Instrumenting Application! - Please Wait ...",
+				true, new IRunnableWithProgress() {
+			@Override
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				monitor.beginTask(
+						"Installing Application" + " " + instrumentrdApkName + " " + " - Please Wait ...", -1);
+
+				try {
+					installApp(instrumentedApkDir, instrumentrdApkName);
+
+				} catch (IOException | InterruptedException e) {
+
+					e.printStackTrace();
+				}
+
+				monitor.done();
+
+			}
+		});
+		msd.closeProgressDialog();
+	}
+
+	public void installApp(String instrumentedApkDir, String instrumentedApkName)
+			throws IOException, InterruptedException {
+		String adbPath = System.getenv("ANDROID_HOME");
+		adbPath = String.valueOf(adbPath) + File.separator + "platform-tools" + File.separator + "adb.exe";
+		String instrumentedApkPath = instrumentedApkDir + File.separator + instrumentedApkName;
+		String commands[] = { adbPath, "install", instrumentedApkPath };
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		pb.directory(new File("C:\\Users\\Arjun\\Desktop\\NeonifyResources"));
+		File log = new File("log");
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(Redirect.appendTo(log));
+		Process process = pb.start();
+		process.waitFor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+		String line;
+		while ((line = br.readLine()) != null) {
+
+			System.out.println(line);
+		}
+
+	}
+
+	public boolean validate(String instrumentorDir, String apkName, String instrumentApkDir,
+			String instrumentedApkName) {
+		if (instrumentorDir.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info",
+					"InstrumentorDirectory cannot be empty");
+			textInstrumentorDir.setFocus();
+			return false;
+		}
+
+		if (apkName.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info", "ApkName cannot be empty");
+			ApkName.setFocus();
+			return false;
+		}
+
+		if (instrumentApkDir.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info",
+					"InstrumentApkDirectory cannot be empty");
+			InstrumentApkDir.setFocus();
+			return false;
+		}
+
+		if (instrumentedApkName.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info",
+					"InstrumentedApk Name cannot be empty");
+			InstrumentedApkName.setFocus();
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean validateInstallation(String instrumentApkDir, String instrumentedApkName) {
+
+		if (instrumentApkDir.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info",
+					"InstrumentApkDirectory cannot be empty");
+			InstrumentApkDir.setFocus();
+			return false;
+		}
+
+		if (instrumentedApkName.equals("")) {
+			MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Info",
+					"InstrumentedApk Name cannot be empty");
+			InstrumentedApkName.setFocus();
+			return false;
+		}
+
+		return true;
+	}
+
+	public void PortForward9899(String port1, String port2) throws IOException, InterruptedException {
+		String command = "cmd /c start cmd.exe /k adb " + " forward" + " tcp:9899" + " tcp:9899";
+		Process process = Runtime.getRuntime().exec(command);
+		process.waitFor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+
+	}
+
+	public void PortForward9898(String port1, String port2) throws IOException, InterruptedException {
+		String command = "cmd /c start cmd.exe /k adb " + " forward" + " tcp:9898" + " tcp:9898";
+		Process process = Runtime.getRuntime().exec(command);
+		process.waitFor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+
+	}
+
+}
